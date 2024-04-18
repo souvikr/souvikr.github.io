ab9b150322492cb0746f63d4fdbed533c5da465b
souvikr <souvikroy5@gmail.com>
2024-04-18 11:19:29 +0530

    changed structure

---
+++ my-react-app/src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <section>
+            <h2>About Me</h2>
+            <p>I'm a web developer with experience in React, Node.js, and more.</p>
+        </section>
+    );
+};
+
+export default About;
---
+++ my-react-app/src/components/Projects.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Projects = () => {
+    return (
+        <section>
+            <h2>Projects</h2>
+            <p>Here are some of my projects:</p>
+            {/* List your projects here */}
+        </section>
+    );
+};
+
+export default Projects;
